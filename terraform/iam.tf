69f20e4c6e31a2025b7d1421bd22c0732f7f256d
Vardhan Yadhav <117582268+vardhan11765@users.noreply.github.com>
2025-06-26 07:05:00 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -70,7 +70,7 @@ resource "aws_iam_role_policy_attachment" "codepipeline_admin_access" {
 }
 
 resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-role-for-static-website"
+  name = "codebuild-role-for-static-website-3"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
